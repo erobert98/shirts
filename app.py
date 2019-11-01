adbc7c8eaec1f4a70a38819b00719d8ee1445687
emile robert <emile.robert123@gmail.com>
2019-11-01 18:42:08 +0000

    asd

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from laneck import app
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5432))
-    app.run(host= '0.0.0.0', port = port, ssl_context='adhoc') 
+    app.run(ssl_context='adhoc') 
